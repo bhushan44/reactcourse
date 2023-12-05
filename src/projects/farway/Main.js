f7f86302dcd52c56221ad48dcd79113e12c41d10
Bhushan345566 <bhushanmandala45@gmail.com>
2023-12-05 19:31:28 +0530

    ok

--- src/projects/farway/Main.js
+++ src/projects/farway/Main.js
@@ -3,12 +3,40 @@ import "./Main.css";
 import { useState } from "react";
 // import { useActionData } from "react-router-dom";
 export default function Main() {
+  const [list, updatelist] = useState([]);
+  const [packed, setpacked] = useState(false);
+  function delete1(id) {
+    updatelist(list.filter((item) => item.id !== id));
+  }
+  function handlecheck(id) {
+    console.log(list);
+    updatelist(
+      list.map((item) => {
+        // console.log(packed);
+        // setpacked((packed) => !packed);
+        // console.log(packed);
+        return item.id === id ? { ...item, packed: !item.packed } : item;
+      })
+    );
+    // console.log(a);
+  }
+
   return (
     <div className="main">
       <Top></Top>
-      <Form></Form>
-      <List></List>
-      <Footer></Footer>
+      <Form
+        list={list}
+        updatelist={updatelist}
+        packed={packed}
+        setpacked={setpacked}
+      ></Form>
+      <List
+        list={list}
+        updatelist={updatelist}
+        onhandlecheck={handlecheck}
+        delete1={delete1}
+      ></List>
+      <Footer list={list}></Footer>
     </div>
   );
 }
@@ -20,20 +48,32 @@ function Top() {
         <span> FAR AWAY</span>
         <span>💼</span>
       </div>
-      <h1></h1>
+      {/* <h1></h1> */}
     </>
   );
 }
-function Form() {
-  const [description, setdes] = useState("0");
+function Form({ list, updatelist, packed, setpacked }) {
+  const [description, setdes] = useState();
   const [quantity, setq] = useState(1);
+  // const [list, updatelist] = useState([]);
+  // const [packed, updatepack] = useState(true);
+  const [id, setid] = useState(1);
   function handleForm(e) {
     e.preventDefault();
   }
+  // function
   function add(e) {
-    let a = [{}];
-    a = [...a, { quantity, description }];
-    console.log(a);
+    // let a = [{}];
+    if (!description) {
+      return;
+    }
+    setid((id) => (id = id + 1));
+    let items = { description, quantity, id, packed: false };
+    console.log(list);
+    updatelist((list) => {
+      return [...list, items];
+    });
+    console.log(list);
     setdes("");
     setq(1);
   }
@@ -46,7 +86,7 @@ function Form() {
             value={quantity}
             onChange={(e) => {
               console.log(e.target.value);
-              setq(e.target.value);
+              setq(Number(e.target.value));
             }}
           >
             {Array.from({ length: 20 }, (_, index) => {
@@ -69,17 +109,96 @@ function Form() {
     </>
   );
 }
-function List() {
+function List({ list, updatelist, onhandlecheck, delete1 }) {
+  const [sortby, updatesortby] = useState("input");
+  // cons [sorteditems,setsorted]=useState([])
+  let sorteditems;
+  function onClear() {
+    let confirm = window.confirm("are you sure want to delete all items?");
+    if (confirm) updatelist([]);
+  }
+
+  console.log(sortby);
+  if (sortby === "input") sorteditems = list;
+  // return;
+  // console.log(items);
+  if (sortby === "quantity") {
+    console.log("before", list);
+    sorteditems = list.slice().sort((a, b) => a.quantity - b.quantity);
+
+    // console.log(items);}
+    console.log("after", sorteditems);
+  } else if (sortby === "packed") console.log(list.packed);
+  sorteditems = list
+    .slice()
+    .sort((a, b) => Number(a.packed) - Number(b.packed));
+  // console.log(items);
+
   return (
     <>
-      <div className="list"></div>
+      <div className="list">
+        <ul className="listitems">
+          {sorteditems.map((items) => {
+            return (
+              <div
+                className="items"
+                key={items.id}
+                style={items.packed ? { textDecoration: "line-through" } : {}}
+              >
+                {/* <h1>{items.packed}</h1> */}
+
+                <input
+                  type="checkbox"
+                  value={items.packed}
+                  onChange={() => onhandlecheck(items.id)}
+                ></input>
+
+                <span>{items.quantity}</span>
+                <span>{items.description}</span>
+                {/* <span>{items.id}</span> */}
+                <button
+                  onClick={() => {
+                    delete1(items.id);
+                  }}
+                >
+                  {" "}
+                  delete
+                </button>
+              </div>
+            );
+          })}
+        </ul>
+        <select
+          value={sortby}
+          onChange={(e) => {
+            updatesortby(e.target.value);
+          }}
+        >
+          <option value="input">sortby input</option>
+          <option value=" packed">sortby packed</option>
+          <option value="quantity">sortby quantity</option>
+        </select>
+        <button onClick={onClear}>clear list</button>
+      </div>
     </>
   );
 }
-function Footer() {
+function Footer({ list }) {
+  let length = list.length;
+  let packeditems = list.filter((item) => item.packed).length;
+  let percentage = Math.round((packeditems / length) * 100);
+
+  console.log(packeditems);
+  console.log(length);
   return (
     <>
-      <div className="footer"></div>
+      <div className="footer">
+        {percentage === 100
+          ? "you got all items and ready to go "
+          : length === 0
+          ? "you have no items in list"
+          : `you have ${length} item in your list and you have already packed ${packeditems} (${percentage}%)`}
+      </div>
     </>
   );
 }
